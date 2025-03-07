fae52b8c77e2af07aafbaee3ac063064829839e7
Ulises <larrabeulises@gmail.com>
2025-03-07 15:28:05 -0300

    Enhance Hero component layout and styling; add dashboard title and sales summary

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,7 +1,17 @@
 import React from "react";
 
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <main className="w-full flex flex-col p-4 gap-4">
+      <h1 className="font-bold text-4xl">Dashboard</h1>
+      <section className="flex gap-4">
+        <article className="p-2 border flex justify-center  flex-col border-gray-300 rounded-lg min-w-3xs min-h-32">
+          <h2 className="text-gray-600">Ventas totales</h2>
+          <p className="font-bold text-3xl">$1000</p>
+        </article>
+      </section>
+    </main>
+  );
 };
 
 export default Hero;
