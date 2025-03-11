7d421d622eb67604d19289cfc3ed0fa55c54c611
Ulises <larrabeulises@gmail.com>
2025-03-11 15:47:14 -0300

    Remove 'Realizar Arqueo' button from HeaderSection component

--- src/components/HeaderSection.tsx
+++ src/components/HeaderSection.tsx
@@ -6,9 +6,6 @@ const HeaderSection = () => {
     <section className="flex justify-between">
       <h1 className="font-bold text-4xl">Arqueo de caja</h1>
       <div className="flex gap-2">
-        <button className="text-eerie-black px-4 py-2 rounded-lg border border-eerie-black cursor-pointer">
-          Realizar Arqueo
-        </button>
         <Link
           href="/caja/retiro"
           className="bg-eerie-black text-white px-4 py-2 rounded-lg cursor-pointer"
