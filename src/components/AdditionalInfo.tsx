2696aee3d0f9f8c41b76b0a8f49b160dcdbfd82b
Ulises <larrabeulises@gmail.com>
2025-03-18 13:23:19 -0300

    Refactor AdditionalInfo and Shortcuts components for improved layout and link integration

--- src/components/AdditionalInfo.tsx
+++ src/components/AdditionalInfo.tsx
@@ -1,14 +1,12 @@
-import React from 'react'
-import { SummaryBar } from './SummaryBar'
-import { Shortcuts } from './Shortcuts'
+import React from "react";
+import { SummaryBar } from "./SummaryBar";
+import { Shortcuts } from "./Shortcuts";
 
 export const AdditionalInfo = () => {
   return (
-    <section className='flex gap-2 w-full'>
-        <SummaryBar/ >
-        <Shortcuts/ >
+    <section className="flex gap-2 w-full">
+      <SummaryBar />
+      <Shortcuts />
     </section>
-
-    
-  )
-}
+  );
+};
--- src/components/Shortcuts.tsx
+++ src/components/Shortcuts.tsx
@@ -1,26 +1,39 @@
+import Link from "next/link";
 import React from "react";
 
 export const Shortcuts = () => {
   return (
-    <article className="p-2 border border-gray-300 rounded-lg flex flex-col gap-2 w-1/2">
+    <article className="p-2 border border-gray-300 rounded-lg flex flex-col gap-6 w-1/2">
       <h3 className="font-bold text-xl">Acciones Rápidas</h3>
       <div className="flex flex-col gap-3 w-full">
         <div className="flex gap-2">
-          <button className="bg-blue-500 text-white rounded-lg p-2 w-1/2">
-            Nueva Pedido
-          </button>
-          <button className="bg-blue-500 text-white rounded-lg p-2 w-1/2">
+          <Link
+            href="/pedidos/crear"
+            className="bg-eerie-black text-snow rounded-lg p-2 w-1/2 flex justify-center items-center"
+          >
+            Nuevo Pedido
+          </Link>
+          <Link
+            href="/inventario"
+            className="bg-snow text-eerie-black rounded-lg p-2 w-1/2 flex justify-center items-center border border-eerie-black"
+          >
             Gestionar Stock
-          </button>
+          </Link>
         </div>
 
         <div className="flex gap-2">
-          <button className="bg-blue-500 text-white rounded-lg p-2 w-1/2">
+          <Link
+            href="/inventario/agregar"
+            className="bg-snow text-eerie-black rounded-lg p-2 w-1/2 flex justify-center items-center border border-eerie-black"
+          >
             Agregar Sabor
-          </button>
-          <button className="bg-blue-500 text-white rounded-lg p-2 w-1/2">
+          </Link>
+          <Link
+            href="/caja/retiro"
+            className="bg-snow text-eerie-black rounded-lg p-2 w-1/2 flex justify-center items-center border border-eerie-black"
+          >
             Registrar Retiro
-          </button>
+          </Link>
         </div>
       </div>
     </article>
