74a5bae64a674bb691a08a29cf69ac43e97b1d7b
Alejo <alejosinger1@gmail.com>
2025-03-18 12:24:22 -0300

    Add AdditionalInfo and SummaryBar components to enhance dashboard details

---
+++ src/components/AdditionalInfo.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { SummaryBar } from './SummaryBar'
+
+export const AdditionalInfo = () => {
+  return (
+    <section>
+        <SummaryBar/ >
+    </section>
+
+    
+  )
+}
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import HeroSummary from "./HeroSummary";
+import { AdditionalInfo } from "./AdditionalInfo";
 
 const Hero = () => {
   return (
     <main className="w-full flex flex-col p-4 gap-4">
       <h1 className="font-bold text-4xl">Dashboard</h1>
-      <HeroSummary />
+      <HeroSummary /> 
+      <AdditionalInfo /> 
     </main>
   );
 };
