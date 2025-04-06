3211fb4f62d2d5109e3b31c25515bd9f1cb44b43
Ulises <larrabeulises@gmail.com>
2025-04-06 16:03:08 -0300

    Add paymentoptions

---
+++ src/consts/paymentsOptions.ts
@@ -0,0 +1,7 @@
+export type payments = "cash" | "mercado_pago" | "card";
+
+export const paymentsOptions = [
+  { placeholder: "Efectivo", value: "cash" },
+  { placeholder: "Mercado pago", value: "mercado_pago" },
+  { placeholder: "Tarjeta", value: "card" },
+];
