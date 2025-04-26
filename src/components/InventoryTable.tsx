631a772dba6d2cafc578f9f6447186a3ac8d41cd
Ulises <larrabeulises@gmail.com>
2025-04-26 12:34:52 -0300

    Modified inventory view

--- src/components/InventoryTable.tsx
+++ src/components/InventoryTable.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { useFlavoursContext } from "@/hooks/useFlavoursContext";
+import { REFRIGERATORS_INFO } from "@/consts/refrigerators";
 import React from "react";
 import TheadBox from "./TheadBox";
 import Tdescription from "./Tdescription";
@@ -7,6 +8,16 @@ import Link from "next/link";
 
 const InventoryTable = () => {
   const { flavours } = useFlavoursContext();
+  console.log(flavours);
+
+  const groupedByRefrigerator: { [key: string]: typeof flavours } = {};
+  REFRIGERATORS_INFO.forEach(({ value }) => {
+    groupedByRefrigerator[value] = flavours.filter(
+      (flavour) => flavour.refrigerator === value
+    );
+  });
+  const noCategory = flavours.filter((flavour) => !flavour.refrigerator);
+
   return (
     <div className="overflow-x-auto">
       <table className="min-w-full divide-y divide-gray-200">
@@ -18,29 +29,78 @@ const InventoryTable = () => {
             <TheadBox title="Acciones" />
           </tr>
         </thead>
-        <tbody className="bg-white divide-y divide-gray-200">
-          {flavours.map((flavour) => (
-            <tr key={flavour._id}>
-              <Tdescription>{flavour.name}</Tdescription>
-              <Tdescription>
-                {flavour.stock === 0 ? (
-                  <span className="text-red-500">Sin stock</span>
-                ) : (
-                  flavour.stock
-                )}
-              </Tdescription>
-              <Tdescription>{flavour.local}</Tdescription>
-              <Tdescription>
-                <Link
-                  href={`/inventario/${flavour._id}`}
-                  className="border-b-2 cursor-pointer"
-                >
-                  Editar
-                </Link>
-              </Tdescription>
+        {REFRIGERATORS_INFO.map(
+          ({ value, title }) =>
+            groupedByRefrigerator[value].length > 0 && (
+              <tbody
+                key={value}
+                className="bg-white divide-y divide-gray-200 border-t-2 border-b-2 border-gray-200"
+              >
+                <tr>
+                  <td
+                    colSpan={4}
+                    className="bg-gray-100 font-bold text-lg px-6 py-2"
+                  >
+                    {title}
+                  </td>
+                </tr>
+                {groupedByRefrigerator[value].map((flavour) => (
+                  <tr key={flavour._id}>
+                    <Tdescription>{flavour.name}</Tdescription>
+                    <Tdescription>
+                      {flavour.stock === 0 ? (
+                        <span className="text-red-500">Sin stock</span>
+                      ) : (
+                        flavour.stock
+                      )}
+                    </Tdescription>
+                    <Tdescription>{flavour.local}</Tdescription>
+                    <Tdescription>
+                      <Link
+                        href={`/inventario/${flavour._id}`}
+                        className="border-b-2 cursor-pointer"
+                      >
+                        Editar
+                      </Link>
+                    </Tdescription>
+                  </tr>
+                ))}
+              </tbody>
+            )
+        )}
+        {noCategory.length > 0 && (
+          <tbody className="bg-white divide-y divide-gray-200 border-t-2 border-b-2 border-gray-200">
+            <tr>
+              <td
+                colSpan={4}
+                className="bg-gray-100 font-bold text-lg px-6 py-2"
+              >
+                Sin categoría (?)
+              </td>
             </tr>
-          ))}
-        </tbody>
+            {noCategory.map((flavour) => (
+              <tr key={flavour._id}>
+                <Tdescription>{flavour.name}</Tdescription>
+                <Tdescription>
+                  {flavour.stock === 0 ? (
+                    <span className="text-red-500">Sin stock</span>
+                  ) : (
+                    flavour.stock
+                  )}
+                </Tdescription>
+                <Tdescription>{flavour.local}</Tdescription>
+                <Tdescription>
+                  <Link
+                    href={`/inventario/${flavour._id}`}
+                    className="border-b-2 cursor-pointer"
+                  >
+                    Editar
+                  </Link>
+                </Tdescription>
+              </tr>
+            ))}
+          </tbody>
+        )}
       </table>
     </div>
   );
