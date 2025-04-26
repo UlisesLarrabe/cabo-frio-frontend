6eb8ebbd845126d530b19385d7b1f7e7fa51827b
Ulises <larrabeulises@gmail.com>
2025-04-26 12:34:40 -0300

    Add refrigerators type

---
+++ src/consts/refrigerators.ts
@@ -0,0 +1,9 @@
+export const REFRIGERATORS_INFO = [
+  { value: "special-cream-mantecol", title: "Cremas Especiales (Mantecol)" },
+  { value: "chocolate", title: "Chocolates" },
+  { value: "sambayon-water", title: "Sambayón y Agua" },
+  { value: "cream-water-lemon", title: "Agua y Crema (Limón)" },
+  { value: "cream-vanilla", title: "Cremas (Vainilla)" },
+  { value: "cream-split", title: "Cremas (Split)" },
+  { value: "ddl", title: "DDL" },
+];
