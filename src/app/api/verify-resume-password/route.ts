355763953cc1743e14e91d628205387c7934d956
Ulises <larrabeulises@gmail.com>
2025-04-26 20:36:23 -0300

    Fix

--- src/app/api/verify-resume-password/route.ts
+++ src/app/api/verify-resume-password/route.ts
@@ -1,4 +1,4 @@
-export async function POST(req: { json: () => any }) {
+export async function POST(req: { json: () => Promise<any> }) {
   const body = await req.json();
   const passwordRequired = process.env.RESUME_PASSWORD;
   if (body.password === passwordRequired) {
